699400e5f27b7fe329cd9e076ea3c0b72602e1eb
Dominique <dominique.lopez@digitregroup.com>
2018-11-12 10:57:25 +0100

    Updated Repository component

--- src/App.js
+++ src/App.js
@@ -85,6 +85,14 @@ const Repository = ({repository}) => (
             <strong>In Repository: </strong>
             <a href={repository.url}>{repository.name}</a>
         </p>
+        <ul>
+            {repository.issues.edges.map(issue => (
+                <li key={issue.node.id}>
+                    <a href={issue.node.url}>{issue.node.title}</a>
+                </li>
+            ))}
+        </ul>
+
     </div>
 );
 
